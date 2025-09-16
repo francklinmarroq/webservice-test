21042f6a0a5e7dc272c8c464c7197a6f10b52c59
Francklin Marroquin <francklin.marroquin@outlook.com>
2025-09-16 01:06:27 -0600

    init commit

---
+++ src/index.js
@@ -0,0 +1,22 @@
+import express from 'express'
+const app = express();
+const port = 3000;
+
+
+app.use(express.json());
+
+
+app.post('/v1/', (req, res) => {
+    const receivedJson = req.body;
+    
+    if (Object.keys(receivedJson).length === 0) {
+        return res.status(400).json({ error: "No se recibió un cuerpo de solicitud JSON válido." });
+    }
+    
+    res.json(receivedJson);
+});
+
+
+app.listen(port, () => {
+    console.log(`Servidor de eco escuchando en http://localhost:${port}`);
+});
