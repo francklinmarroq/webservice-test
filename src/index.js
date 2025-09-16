30a8c888353a58f53d3852f6c3f61b7004b4a2c6
Francklin Marroquin <francklin.marroquin@outlook.com>
2025-09-16 12:46:05 -0600

    returning status codes based on parameters

--- src/index.js
+++ src/index.js
@@ -1,22 +1,35 @@
-import express from 'express'
+import express, { json } from 'express'
 const app = express();
 import dotenv from 'dotenv'
 
 
 app.use(express.json());
-dotenv.config()
+dotenv.config();
 
 const port = process.env.PORT;
 
 app.get('/v1/', (req, res) => {
 
-    
-   
+    const hubMode = req.query["hub.mode"];
+    const hubChallenge = req.query["hub.challenge"];
+    const verifyToken = req.query["hub.verify_token"];
 
-    const verifyToken = req.query.hub_verify_token;
+    if(hubMode != undefined && hubChallenge !=  undefined && verifyToken != undefined){
+        if (hubMode === 'subscribe' && verifyToken === process.env.VERIFY_TOKEN){
+            console.log("Webhook verified. ", hubChallenge);
+            res.status(200);
+            res.send(hubChallenge);
+        } else {
+            res.status(404);
+            res.json("WRONG VERIFY_TOKEN");
+        }
+    }else{
+        res.status(404);
+        res.json("Something went wrong. Check that all parameters are being passed.")
+    }
+
+
     
-    console.log(req.query);
-    console.log(verifyToken);
     
 });
 
